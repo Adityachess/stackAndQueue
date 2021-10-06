81b27eae686dbf48794ff2dc90e9a06bff6f9b5e
Adityachess <adityadone9398@gmail.com>
2021-10-06 17:53:48 +0530

    ADD:Welocme message.

---
+++ main/java/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stackandqueue;
+
+
+public class StackAndQueue 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to stack And Queue Program" );
+    }
+}
